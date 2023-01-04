a7e9c2305fbf8164889ae02db6d111c0626e8cac
oyune <yooune.01@gmail.com>
2023-01-04 12:02:33 +0900

    entity 90%

--- src/main/java/com/untouchable/everytime/Entity/LectureEntity.java
+++ src/main/java/com/untouchable/everytime/Entity/LectureEntity.java
@@ -1,10 +1,13 @@
 package com.untouchable.everytime.Entity;
 
+import com.untouchable.everytime.Enum.LectureType;
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+
+import java.util.ArrayList;
 
 @Entity
 @Data
@@ -12,15 +15,27 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 @AllArgsConstructor
 
-public class Lecture {
+public class LectureEntity {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     Long LPK;
 
+    @Enumerated(EnumType.STRING)
+    LectureType lectureType;
+
     @Column
     int lectureNum;
     int separation;
-    String lecutureName;
+    String lectureName;
+    int credit;
+    int grade;
+    String professor;
+
+    @ManyToMany
+    ArrayList<BookEntity> book;
+
+    @ManyToOne
+    ScheduleEntity schedule;
 
 }
---
+++ src/main/java/com/untouchable/everytime/Entity/LectureScheduleEntity.java
@@ -0,0 +1,27 @@
+package com.untouchable.everytime.Entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToMany;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.ArrayList;
+
+@Entity
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+
+public class LectureScheduleEntity {
+    @Id
+    Long PK;
+
+    @ManyToMany
+    ArrayList<LectureEntity> lectureName;
+
+
+}
