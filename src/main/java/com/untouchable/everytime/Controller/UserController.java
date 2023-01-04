d2acc0f8896a458ef49d9c01967217f5e4f1c1e1
Namju Kim <cmsong111@naver.com>
2023-01-04 20:09:16 +0900

    add swagger

--- src/main/java/com/untouchable/everytime/Controller/UserController.java
+++ src/main/java/com/untouchable/everytime/Controller/UserController.java
@@ -5,14 +5,20 @@ import com.untouchable.everytime.DTO.UserDTO;
 import com.untouchable.everytime.Entity.UserEntity;
 import com.untouchable.everytime.Enum.AttachmentType;
 import com.untouchable.everytime.Service.UserService;
+
+import io.swagger.v3.oas.annotations.*;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.persistence.*;
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Data;
-import lombok.NoArgsConstructor;
+import lombok.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
+@Tag(name = "유저", description = "유저 정보 왔다갔다 하는 API")
 @RestController
 @RequestMapping("/user")
 public class UserController {
@@ -41,14 +47,21 @@ public class UserController {
     }
 
     @PatchMapping("/update")
-    public UserDTO update() {
+    public UserDTO update(@RequestBody UserDTO userDTO) {
         // TODO : 해야함
         UserDTO user = new UserDTO();
         return user;
     }
 
     @DeleteMapping("/delete")
-    public UserDTO delete() {
+    public UserDTO delete(@RequestBody UserDTO userDTO) {
+        // TODO : 해야함
+        UserDTO user = new UserDTO();
+        return user;
+    }
+
+    @GetMapping("/info")
+    public UserDTO info(@RequestBody UserDTO userDTO) {
         // TODO : 해야함
         UserDTO user = new UserDTO();
         return user;
