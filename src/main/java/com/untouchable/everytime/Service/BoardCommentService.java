74172b5e4c101497c37f4168355db311f76ebe19
Namju Kim <cmsong111@naver.com>
2023-01-07 17:12:28 +0900

    댓글 서비스 수정

--- src/main/java/com/untouchable/everytime/Service/BoardCommentService.java
+++ src/main/java/com/untouchable/everytime/Service/BoardCommentService.java
@@ -4,13 +4,16 @@ import com.untouchable.everytime.Config.JwtConfig;
 import com.untouchable.everytime.DTO.BoardCommentDTO;
 import com.untouchable.everytime.Entity.BoardCommentEntity;
 import com.untouchable.everytime.Entity.BoardEntity;
+import com.untouchable.everytime.Entity.UserEntity;
 import com.untouchable.everytime.Repository.BoardCommentRepository;
 import com.untouchable.everytime.Repository.BoardRepository;
+import com.untouchable.everytime.Repository.UserRepository;
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
+import java.sql.Timestamp;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
@@ -21,32 +24,36 @@ public class BoardCommentService {
 
     BoardCommentRepository boardCommentRepository;
     BoardRepository boardRepository;
+
+    UserRepository userRepository;
     ModelMapper modelMapper;
     JwtConfig jwtConfig;
 
     @Autowired
-    public BoardCommentService(BoardRepository boardRepository, BoardCommentRepository boardCommentRepository, ModelMapper looseMapper, JwtConfig jwtConfig) {
+    public BoardCommentService(UserRepository userRepository,BoardRepository boardRepository, BoardCommentRepository boardCommentRepository, ModelMapper looseMapper, JwtConfig jwtConfig) {
         this.boardCommentRepository = boardCommentRepository;
         this.modelMapper = looseMapper;
         this.jwtConfig = jwtConfig;
         this.boardRepository = boardRepository;
+        this.userRepository = userRepository;
     }
 
     public ResponseEntity<BoardCommentDTO> createBoardComment(BoardCommentDTO boardCommentDTO, String token) {
         Map<String, Object> jwt = jwtConfig.verifyJWT(token);
+        BoardCommentEntity boardCommentEntity = modelMapper.map(boardCommentDTO, BoardCommentEntity.class);
+
+        //게시글 유효한지 확인
         Optional<BoardEntity> boardEntity = boardRepository.findById(boardCommentDTO.getBoardPK());
-
-
-
-        boardCommentDTO.setUserID((String) jwt.get("userID"));
-        BoardCommentEntity boardCommentEntity =  modelMapper.map(boardCommentDTO, BoardCommentEntity.class);
-
-
         if (!boardEntity.isPresent()) {
             ResponseEntity.notFound().build();
-        } else if (!boardEntity.get().getSchool().equals(jwt.get("ID"))) {
-            ResponseEntity.badRequest().build();
         }
+
+        // User 정보 매핑
+        UserEntity userEntity = userRepository.findById(String.valueOf(jwt.get("ID"))).get();
+        boardCommentEntity.setUser(userEntity);
+        boardCommentEntity.setBoard(boardEntity.get());
+        boardCommentEntity.setCreatedAT(new Timestamp(System.currentTimeMillis()));
+
 
         BoardCommentEntity resultEntity = boardCommentRepository.save(boardCommentEntity);
 
@@ -82,22 +89,27 @@ public class BoardCommentService {
 
     public ResponseEntity<ArrayList<BoardCommentDTO>> getBoardCommentByBoardID(Long id, String token) {
         Map<String, Object> jwt = jwtConfig.verifyJWT(token);
+
+        //게시글 유효한지 확인
         Optional<BoardEntity> boardEntity = boardRepository.findById(id);
-
-        if (boardEntity.isPresent() && boardEntity.get().getSchool().getSchoolName().equals(jwt.get("SCHOOL"))) {
-            List<BoardCommentEntity> result = boardCommentRepository.findByBoard_BoardPK(id);
-
-            ArrayList<BoardCommentDTO> resultDTO = new ArrayList<>();
-
-            for (BoardCommentEntity boardCommentEntity : result) {
-                resultDTO.add(modelMapper.map(boardCommentEntity, BoardCommentDTO.class));
-            }
-
-            return ResponseEntity.ok(resultDTO);
-
-        } else {
-            return ResponseEntity.badRequest().build();
+        if (!boardEntity.isPresent()) {
+            return ResponseEntity.notFound().build();
         }
+
+//        //게시글 소속 학교가 토큰이랑 맞는지 확인
+//        if (boardEntity.get().getSchool().getSchoolName().equals(String.valueOf(jwt.get("SCHOOL")))) {
+//            return ResponseEntity.badRequest().build();
+//        }
+
+        List<BoardCommentEntity> result = boardCommentRepository.findByBoard_BoardPK(id);
+
+        ArrayList<BoardCommentDTO> resultDTO = new ArrayList<>();
+
+        for (BoardCommentEntity boardCommentEntity : result) {
+            resultDTO.add(modelMapper.map(boardCommentEntity, BoardCommentDTO.class));
+        }
+
+        return ResponseEntity.ok(resultDTO);
 
     }
 
