ed9e4228cf206a7242f573ad46301d88bfe7cb23
Namju Kim <cmsong111@naver.com>
2023-01-23 22:15:33 +0900

    make test Profile & fix Entity

--- src/main/java/com/untouchable/everytime/Schedule/Entity/ScheduleEntity.java
+++ src/main/java/com/untouchable/everytime/Schedule/Entity/ScheduleEntity.java
@@ -23,12 +23,12 @@ public class ScheduleEntity {
     String location;
 
     // 일요일 0 ~ 토요일 6
+    @Column(name = "Schedule_Day")
     int day;
 
     // 0시 ~ 24시 , start < end
+    @Column(name = "start_time")
     int start;
+    @Column(name = "end_time")
     int end;
-
-
-
 }
